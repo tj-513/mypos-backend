dc0d32ca47149ffe6ff31ab0f1b18f42c61812e2
tharindur <tranaweera755@gmail.com>
2019-03-19 00:18:35 +0530

    add put as allowed method

--- src/main/java/com/bootcamp/mypos/mypos/common/SimpleCORSFilter.java
+++ src/main/java/com/bootcamp/mypos/mypos/common/SimpleCORSFilter.java
@@ -25,7 +25,7 @@ public class SimpleCORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
